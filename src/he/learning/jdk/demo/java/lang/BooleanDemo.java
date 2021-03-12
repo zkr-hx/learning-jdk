e454f04f94b5e53e1d08a7bdefadd76918b7d487
zhukerui <m13057531285@163.com>
2021-03-12 17:09:29 +0800

    完善BooleanDemo

--- src/he/learning/jdk/demo/java/lang/BooleanDemo.java
+++ src/he/learning/jdk/demo/java/lang/BooleanDemo.java
@@ -1,8 +1,16 @@
 package he.learning.jdk.demo.java.lang;
 
 /**
- * @author: 禾绪
+ * @author 禾绪
  */
 public class BooleanDemo {
+
+    public static void main(String[] args) {
+        System.out.println(Boolean.TYPE);
+        System.out.println(new Boolean("TRUE"));
+        System.out.println(new Boolean("tru"));
+        System.out.println(new Boolean(""));
+        System.out.println(new Boolean(null));
+    }
 
 }
